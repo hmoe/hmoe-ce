e9784a705e1c974427216826b3d31262250a6a4f
manageryzy <manageryzy@gmail.com>
2015-11-26 13:42:18 +0800

    数据接口完成，等做完前端就回老家结婚

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var userdata = require('./routes/userdata');
+var video = require('./routes/video');
+var videodata = require('./routes/videodata')
 
 var app = express();
 
@@ -24,6 +27,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/userdata', userdata);
+app.use('/video', video);
+app.use('/videodata', videodata);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/userdata.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+var mysql      = require('mysql');
+
+/* GET users listing. */
+router.get(/\d+/, function(req, res, next) {
+	var id = parseInt(req.url.substring(1));
+  var connection = mysql.createConnection(global.db);
+  connection.connect();
+  connection.query("SELECT * FROM up_history WHERE mid = ?",[id],function(err,rows,fields){
+    if(err)throw err;
+    
+    res.send(JSON.stringify(rows));
+    connection.end();
+  });
+  
+});
+
+module.exports = router;
